6fa2d65d56be2430dfb574822678340986419432
Arlind Musliu <arlindmusliu90@gmail.com>
2020-04-26 15:11:54 +0200

    added img to mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss')
 
 /*
  |--------------------------------------------------------------------------
@@ -12,11 +13,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
-    
-const tailwindcss = require('tailwindcss')
 
 mix.sass('resources/sass/app.scss', 'public/css')
    .options({
       processCssUrls: false,
       postCss: [ tailwindcss('tailwind.config.js') ],
 })
+
+mix.copy('resources/img/', 'public/storage');
