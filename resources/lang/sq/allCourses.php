531d21f614cf4413cd4608676e186e317693332d
Arlind Musliu <arlindmusliu90@gmail.com>
2020-06-01 15:19:15 +0200

    added translations for Macedonian

---
+++ resources/lang/sq/allCourses.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | All Courses from the Home page
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines display the courses section in the homepage.
+    |
+    */
+
+    'learn_front_end' => 'Mëso ueb programim front-end & back-end në shpejtësinë që të konvenon',
+    'learn_high_level' => 'Mëso aftësi të thella të programimit duke përdorur gjuhët programuse të nivelit të lartë.',
+    'create_advanced_functional' => 'Krijoni programe dhe lojra të ndryshme për kompjuter ose telefon.',
+    'learn_the_fastest' => 'Mëso gjuhën programuese e cila zhvillohet më së shpejti.',
+    'front_end_web' => 'Front-end programimi asnjëherë nuk ka qenë më i lehtë me kurset tona për HTML & CSS.',
+    'learn_the_basics' => 'Mëso bazat e përdorimit të përgjithshëm të gjuhëve programuese.',
+    'apps_and_games' => 'Aplikacione & Lojra',
+    'intro_to_coding' => 'Hyrje në Programim',
+    
+];
--- resources/views/partials/all_courses.blade.php
+++ resources/views/partials/all_courses.blade.php
@@ -5,21 +5,21 @@
                 <img src="{{ asset('/storage/webdevelopment_img.png') }}" alt="webdevelopment">
                 <div class="ml-4">
                     <h2 class="text-2xl font-bold">Web Development</h2>
-                    <p> Learn front-end & back-end wev developing at the convenient speed for you </p>
+                    <p>{{ __('allCourses.learn_front_end') }}</p>
                 </div>
             </div>
             <div class="flex flex-row items-center mb-12">
                 <img src="{{ asset('/storage/python_Img.png') }}" alt="python">
                 <div class="ml-4">
                     <h2 class="text-2xl font-bold">Python</h2>
-                    <p>Learn high-level programming skills using a high language.</p>
+                    <p>{{ __('allCourses.learn_high_level') }}</p>
                 </div>
             </div>
             <div class="flex flex-row items-center mb-12">
                 <img src="{{ asset('/storage/apps_img.png') }}" alt="apps_games">
                 <div class="ml-4">
-                    <h2 class="text-2xl font-bold">Apps & Games </h2>
-                    <p>Create advanced functional desktop and mobile apps & games.</p>
+                    <h2 class="text-2xl font-bold">{{ __('allCourses.apps_and_games') }}</h2>
+                    <p>{{ __('allCourses.create_advanced_functional') }}</p>
                 </div>
             </div>
         </div>
@@ -28,21 +28,21 @@
                 <img src="{{ asset('/storage/js_img.png') }}" alt="JS">
                 <div class="ml-4">
                     <h2 class="font-bold text-2xl">Javascript</h2>
-                    <p>Learn the fastest growing web developing programming language.</p>
+                    <p>{{ __('allCourses.learn_the_fastest') }}</p>
                 </div>
             </div>
             <div class="flex flex-row items-center mb-12">
                 <img src="{{ asset('/storage/htmlcss_img.png') }}" alt="HTMLCSS">
                 <div class="ml-4">
                     <h2 class="font-bold text-2xl">HTML & CSS</h2>
-                    <p>Front-end web developing hasn't been easier using our HTML & CSS courses.</p>
+                    <p>{{ __('allCourses.front_end_web') }}</p>
                 </div>
             </div>
             <div class="flex flex-row items-center mb-12">
                 <img src="{{ asset('/storage/intcode_img.png') }}" alt="Introtocode">
                 <div class="ml-4">
-                    <h2 class="font-bold text-2xl">Intro to Coding</h2>
-                    <p>Learn the basics of the general use of all programming languages.</p>
+                    <h2 class="font-bold text-2xl">{{ __('allCourses.intro_to_coding') }}</h2>
+                    <p>{{ __('allCourses.learn_the_basics') }}</p>
                 </div>
 
             </div>
