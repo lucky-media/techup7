490174160783ecc2db1696a676f978130cdec7d5
Arlind Musliu <arlindmusliu90@gmail.com>
2020-06-07 16:15:42 +0200

    Emails are queued on the database, less waiting

--- app/Mail/ContactFormEmail.php
+++ app/Mail/ContactFormEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ContactFormEmail extends Mailable
+class ContactFormEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
